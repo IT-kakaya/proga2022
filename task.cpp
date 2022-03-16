fabb17f83a584c94e39387ea18dd8ea3ca0f9571
IT-kakaya <99839986+IT-kakaya@users.noreply.github.com>
2022-03-17 00:47:26 +0300

    Create task.cpp

---
+++ task.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+string TheAWord(string str1){ 
+    string str2;
+    str2.clear();
+    if (str1[0] == 'A'){
+        for (int j = 0; j < str1.size() - 1; j++){
+            if (str1[j] != ' '){
+                str2.push_back(str1[j]);
+                }
+            else{
+                break;
+            }
+        }
+        str2.push_back(' ');
+    }
+    for (int i = 1; i < str1.size() - 1; i++){
+        if (str1[i] == 'A' && str1[i-1] == ' '){
+            for (int j = i; j < str1.size() - 1; j++){
+                if (str1[j] != ' '){
+                    str2.push_back(str1[j]);
+                }
+                else{
+                    break;
+                }
+            }
+            str2.push_back(' ');
+        }
+    }
+    return str2;
+}
+
+int main()
+{
+    string str1;
+    cout << "Enter the string:" << endl;
+    getline(cin, str1);
+    
+    cout << "Words with the first letter A:" << endl << TheAWord(str1) << endl;
+}
