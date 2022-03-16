e8e1e85b41eb36b4333f45881c66b1caae45db63
IT-kakaya <99839986+IT-kakaya@users.noreply.github.com>
2022-03-16 23:50:50 +0300

    Update taskChess.cpp
    Переделано (из мейна сделаны функции)

--- taskChess.cpp
+++ taskChess.cpp
@@ -1,46 +1,44 @@
 #include <iostream>
 using namespace std;
 
+char Step1(char a, int i){
+    if (char(a) > 'a'){
+        cout << char(a-1) << i << endl; 
+    }
+    if (char(a) < 'g'){
+        cout << char(a+1) << i << endl;
+    } 
+}
+
+char Step2(char a, int i){
+    if (char(a) > 'b'){
+        cout << char(a-2) << i << endl;
+    }
+    if (char(a) < 'f'){
+        cout << char(a+2) << i << endl;
+    }
+}
+
 int main(){
     char a;
     int b;
     int i;
-    cout<<"Enter the cell: ";
-    cin>>a;
-    cin>>b;
-    cout<<"Cells you can go to:"<<endl;
-    for(i=1; i<=8; i++){
-        if (i==b-2 && b>2){
-            if (char(a)>'a'){
-                cout<<char(a-1)<<i<<endl;
-            }
-            if (char(a)<'g'){
-                cout<<char(a+1)<<i<<endl;
-            }
+    cout << "Enter the cell: ";
+    cin >> a;
+    cin >> b;
+    cout << "Cells you can go to:" << endl;
+    for(i = 1; i <= 8; i++){
+        if (i == b - 2 && b > 2){
+            Step1(a, i);
         }
-        if (i==b-1 && b>1){
-            if (char(a)>'b'){
-                cout<<char(a-2)<<i<<endl;
-            }
-            if (char(a)<'f'){
-                cout<<char(a+2)<<i<<endl;
-            }
+        if (i == b - 1 && b > 1){
+            Step2(a, i);
         }
-        if (i==b+2 && b<7){
-            if (char(a)>'a'){
-                cout<<char(a-1)<<i<<endl;
-            }
-            if (char(a)<'g'){
-                cout<<char(a+1)<<i<<endl;
-            }
+        if (i == b + 2 && b < 7){
+            Step1(a, i);
         }
-        if (i==b+1 && b<8){
-            if (char(a)>'b'){
-                cout<<char(a-2)<<i<<endl;
-            }
-            if (char(a)<'f'){
-                cout<<char(a+2)<<i<<endl;
-            }
+        if (i == b + 1 && b < 8){
+            Step2(a, i);
         }
     }
 }
