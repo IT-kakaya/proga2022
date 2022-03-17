025d8e87b5501d50fa1c19b44509039b342efd6a
IT-kakaya <99839986+IT-kakaya@users.noreply.github.com>
2022-03-17 15:55:59 +0300

    Update taskhw5.cpp
    Здравствуйте! В задании было написано выводить слова, начинающие с "А", а Вы вводите с "а". Я добавила в условия строчную букву.

--- taskhw5.cpp
+++ taskhw5.cpp
@@ -4,7 +4,7 @@ using namespace std;
 string TheAWord(string str1){ 
     string str2;
     str2.clear();
-    if (str1[0] == 'A'){
+    if (str1[0] == 'A' || str1[0] == 'a'){
         for (int j = 0; j < str1.size() - 1; j++){
             if (str1[j] != ' '){
                 str2.push_back(str1[j]);
@@ -16,7 +16,7 @@ string TheAWord(string str1){
         str2.push_back(' ');
     }
     for (int i = 1; i < str1.size() - 1; i++){
-        if (str1[i] == 'A' && str1[i-1] == ' '){
+        if ((str1[i] == 'A' || str1[i] == 'a') && str1[i-1] == ' '){
             for (int j = i; j < str1.size() - 1; j++){
                 if (str1[j] != ' '){
                     str2.push_back(str1[j]);
