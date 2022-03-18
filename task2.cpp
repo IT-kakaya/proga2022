1631e9ab4837ead243b100e6733e55edeb67c8dc
IT-kakaya <99839986+IT-kakaya@users.noreply.github.com>
2022-03-18 12:56:06 +0300

    Update task2.cpp
    Добавила пробелы между операциями

--- task2.cpp
+++ task2.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 float Square(float a, float b){
-    return a*b;
+    return a * b;
 }
 
 int main(){
     float a, b;
-    cin>>a;
-    cin>>b;
-    cout<<Square(a, b)<<endl;
+    cin >> a;
+    cin >> b;
+    cout << Square(a, b) << endl;
 }
