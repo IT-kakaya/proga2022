b1f331a947c4c8d2d5e04bea685150396bd75177
IT-kakaya <99839986+IT-kakaya@users.noreply.github.com>
2022-03-18 13:07:04 +0300

    Update task2.cpp
    Это решение продублировано в HW3.cpp, так как было задано повторно. Но я рассчитываю на творческие баллы, потому что старательно выполняю каждое дз, поэтому делаю исправления и в ветке с HW2.cpp, в которой первоначально было это задание)

--- task2.cpp
+++ task2.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int PositiveNumbers(int a){
-    int negative=0;
-    if (a<0){
-        negative=negative+1;
+int PositiveOrNegative(int a){ //Функция возвращает 0, если все числа положительные, и 1, если не все числа положительные 
+    int negative = 0;
+    if (a < 0){
+        negative = 1;
     }
     return negative;
 }
 
 int main(){
-    int amount=0;
+    int amount = 0;
     int a;
-    cout<<"Enter numbers. If you want to stop, enter 0:"<<endl;
-    while(char(a)!=13){
-        cin>>a;
-        PositiveNumbers(a);
-        amount=amount+PositiveNumbers(a);
+    cout << "Enter numbers. If you want to stop, enter 0:" << endl;
+    while (a != 0){
+        cin >> a;
+        amount = amount + PositiveOrNegative(a);
     }
-    if (amount!=0){
-        cout<<"Not all numbers are positive"<<endl;
+    if (amount != 0){
+        cout << "Not all numbers are positive" << endl;
     }
-    else cout<<"All numbers are positive"<<endl;
+    else{
+        cout << "All numbers are positive" << endl;
+    }
 }
-//Оно не работает так, как нужно (числа не перестают вводиться с пробелом, но если ввод закончить, то остальная часть работает, проблема только с остановкой ввода чисел), но я старалась, не придумала ничего другого
+
