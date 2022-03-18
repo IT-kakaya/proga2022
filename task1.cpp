7373d819673b347b360705a74fc08c7a495c5926
IT-kakaya <99839986+IT-kakaya@users.noreply.github.com>
2022-03-18 12:55:23 +0300

    Update task1.cpp
    Добавила пробелы между операциями

--- task1.cpp
+++ task1.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 float Conversion(float x){
-    return x*2.54;
+    return x * 2.54;
 }
 
 int main(){
     float x;
-    cin>>x;
-    cout<<Conversion(x)<<endl;
+    cin >> x;
+    cout << Conversion(x) << endl;
 }
