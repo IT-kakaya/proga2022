667056bf000774fe74c6ce1289c712ba76e6ca31
IT-kakaya <99839986+IT-kakaya@users.noreply.github.com>
2022-05-04 22:15:39 +0300

    Create n2.cpp

---
+++ n2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+string parol(string login1, string login, string password1){
+    string password;
+    for(int i = 0; i<3; i++){
+        cout << "Введите пароль" << endl;
+        cin >> password;
+        if(password1 == password)
+            i = 3;
+    }
+    if (password != password1 || login != login1)
+        cout << "Неверный пароль или логин";
+    return "Пароль подошел";
+}
+
+int main() {
+    string login1 = "mariach";
+    string password1 = "jsm";
+    string login;
+    cout << "Введите логин" << endl;
+    cin >> login;
+    try{
+        cout << parol(login1, login, password1);
+    }
+    catch(...){
+        cout << "Неправильный пароль или логин";
+    }
+}
